c2a8b4ed306f7c556a7c78fc1a0aa614765441d3
EdiblePC <ursvalboi@gmail.com>
2024-12-10 19:55:18 +0100

    Character Controller i chodzenie

--- Assets/Sandbox/InputKeybinds.cs
+++ Assets/Sandbox/InputKeybinds.cs
@@ -1,6 +1,15 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InputKeybinds : MonoBehaviour
 {
-    
+    public KeyCode testKey;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(testKey))
+        {
+            Debug.Log("dupa");
+        }
+    }
 }
