d2fe3e0ae048d6702631d65f9ca3146651e11c58
Auri <akraszczynska0@gmail.com>
2025-05-29 16:35:13 +0200

    poprawa dzwieku exit lvl

--- Assets/Audio/SoundManager.cs
+++ Assets/Audio/SoundManager.cs
@@ -1,12 +1,14 @@
 using System.Collections.Generic;
-using FMOD.Studio;
+using FMOD.Studio;          // <- potrzebne do EventInstance + STOP_MODE
+using FMODUnity;           // <- RuntimeManager, StudioEventEmitter
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager Instance;
 
-    private List<EventInstance> activeEvents = new List<EventInstance>();
+    private readonly List<EventInstance> activeEvents = new();
 
     private void Awake()
     {
@@ -21,21 +23,49 @@ public class SoundManager : MonoBehaviour
         }
     }
 
+    /* ------------------------------------------------------------ */
+    /*  TWORZENIE I REJESTRACJA EVENTÓW                             */
+    /* ------------------------------------------------------------ */
     public EventInstance CreateAndStartEvent(string path)
     {
-        EventInstance instance = FMODUnity.RuntimeManager.CreateInstance(path);
+        EventInstance instance = RuntimeManager.CreateInstance(path);
         instance.start();
         activeEvents.Add(instance);
         return instance;
     }
 
+    /* ------------------------------------------------------------ */
+    /*  ZATRZYMANIE ABSOLUTNIE WSZYSTKIEGO W OKREŒLONYCH SCENACH    */
+    /* ------------------------------------------------------------ */
     public void StopAllActiveEvents()
     {
-        foreach (var instance in activeEvents)
+        string currentScene = SceneManager.GetActiveScene().name;
+        if (currentScene != "NewGeneratorScene" && currentScene != "TutorialScene")
+            return;
+
+        /* 1) EventInstance’y tworzone rêcznie ------------------------ */
+        foreach (var e in activeEvents)
         {
-            instance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-            instance.release();
+            e.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+            e.release();
         }
         activeEvents.Clear();
+
+        /* 2) Wszystkie StudioEventEmitter’y w hierarchii ------------- */
+#if UNITY_2023_1_OR_NEWER
+        var emitters = Object.FindObjectsByType<StudioEventEmitter>(
+            UnityEngine.FindObjectsInactive.Include,
+            UnityEngine.FindObjectsSortMode.None);
+#else
+    var emitters = Object.FindObjectsByType<StudioEventEmitter>(
+        UnityEngine.FindObjectsSortMode.None, true);
+#endif
+        foreach (var emitter in emitters)
+            emitter.Stop();
+
+        /* 3) Ostateczne wyciszenie przez master bus ------------------ */
+        RuntimeManager.StudioSystem.flushCommands();          // dopchnij komendy
+        var masterBus = RuntimeManager.GetBus("bus:/");       // g³ówny bus
+        masterBus.stopAllEvents(FMOD.Studio.STOP_MODE.IMMEDIATE);
     }
 }
